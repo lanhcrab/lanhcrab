4e7431f9a1ba06d81de28d562350dd84cf28e299
lanhcrab <lanhpham301295@gmail.com>
2022-07-26 17:18:51 +0700

    frontend

---
+++ fe/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+     <Login />
+  );
+}
+
+export default App;
---
+++ fe/src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Login.css';
+
+export default function Login() {
+  return (
+    <React.Fragment>
+        <div className="login-background">
+          <div className='login-container'>
+            <img src='image_logo_bts' alt='logo'/>
+            <h6 className='jss6'>HYBE Co.</h6>
+          </div>
+          
+          <div className='login-content'>
+            <div className='text-center'>Chào mừng trở lại!</div>
+            
+            <div className='text-username'>
+              <p>Tên đăng nhập/E-mail</p>
+              <input type="text" className="rectangle" />
+            </div>
+
+            <div className='text-password'>
+              <p>Mật khẩu</p>
+              <input type="password" className="rectangle" />
+
+              <div className='text-forgot'>
+                <span>Quên mật khẩu?</span>
+              </div>
+            
+            </div>
+
+            
+
+            <div className='btn-rectangle'>
+              <button className='btn-login'>ĐĂNG NHẬP</button>
+            </div>
+            
+
+          
+          
+          
+          </div>
+          
+            
+        </div>
+    </React.Fragment>
+  )
+}
